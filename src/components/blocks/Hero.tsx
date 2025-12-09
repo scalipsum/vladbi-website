4244d58887377d9011602f97f8be3604012e3db4
Vlad Bibire <vladbbr1@gmail.com>
2025-12-09 16:57:18 +0200

    Globe added

--- src/components/blocks/Hero.tsx
+++ src/components/blocks/Hero.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import Link from 'next/link';
+import RotatingGlobe from '../RotatingGlobe';
 import Stripes from '../Stripes';
 
 export default async function Hero() {
@@ -9,9 +10,9 @@ export default async function Hero() {
 			id="hero"
 			className="flex flex-row items-center justify-between"
 		>
-			<div className="max-w-1/2">
+			<div className="max-w-1/2 z-10">
 				<h1 className="text-6xl font-main font-extrabold tracking-tight text-brand-500">
-					I build SaaS <br /> products that Scale
+					I build AI SaaS <br /> products that Scale
 				</h1>
 				<p className="font-sans tracking-tight text-xl mt-8">
 					Handling full technical execution. Then <br /> automate
@@ -22,6 +23,11 @@ export default async function Hero() {
 				</Button>
 			</div>
 			<div className="relative">
+				<RotatingGlobe
+					className="absolute top-0 right-10 opacity-100 z-0"
+					size={700}
+				/>
+
 				<Image
 					src="/img/person.png"
 					alt="Vladbi Silhouette"
@@ -29,6 +35,7 @@ export default async function Hero() {
 					height={813}
 					className="relative w-[430px] mr-20 z-20 mt-20"
 				/>
+
 				<Stripes className="absolute top-0 right-0 z-0" />
 			</div>
 		</section>
