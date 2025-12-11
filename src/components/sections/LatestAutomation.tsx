ae2c78de34450996bf1b068217257169bf0fc2f2
Vlad Bibire <vladbbr1@gmail.com>
2025-12-11 15:03:45 +0200

    Card design

--- src/components/sections/LatestAutomation.tsx
+++ src/components/sections/LatestAutomation.tsx
@@ -1,3 +1,38 @@
+import Text from '@/components/ui/text';
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import { Card } from '../ui/card';
+
 export default function LatestAutomation() {
-	return <div>Latest automation</div>;
+	return (
+		<section className="mt-14 pb-14">
+			<Text type="h2">Latest automation</Text>
+
+			<Card className="px-8 py-6 mt-8">
+				<Link
+					href={`/blog/hello`}
+					className="absolute inset-0 z-10"
+					aria-label="Title"
+				/>
+				<div className="flex flex-row items-center">
+					<div className="w-1/3 flex flex-col items-start">
+						<Text type="h3">Blog Posting AI Agent</Text>
+						<Text className="mt-2">For an SEO Agency</Text>
+					</div>
+					<div className="w-1/3 flex justify-center ml-4">
+						<Text>
+							Page layouts look better with something in each
+							section. Web page designers, content writers like
+							this.
+						</Text>
+					</div>
+					<div className="w-1/3 flex justify-end">
+						<Button asChild className="self-end">
+							<Link href="#get-started">View project</Link>
+						</Button>
+					</div>
+				</div>
+			</Card>
+		</section>
+	);
 }
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -1,92 +1,92 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-function Card({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card"
-      className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
-        className
-      )}
-      {...props}
-    />
-  );
+function Card({ className, ...props }: React.ComponentProps<'button'>) {
+	return (
+		<button
+			data-slot="card"
+			className={cn(
+				'relative bg-card text-card-foreground border border-brand-100 dark:border-brand-500 rounded-lg shadow-[0_6px_0_0_#deebf8] dark:shadow-[0_6px_0_0_#043e6d] hover:shadow-[0_8px_0_0_##deebf8] dark:hover:shadow-[0_8px_0_0_#043e6d] hover:translate-y-[-4px] active:shadow-[0_0px_0_0_##deebf8] dark:active:shadow-[0_0px_0_0_#043e6d] dark:hover:translate-y-[-4px]  active:translate-y-[6px] dark:active:translate-y-[6px] transition-all duration-150',
+				className,
+			)}
+			{...props}
+		/>
+	);
 }
 
-function CardHeader({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card-header"
-      className={cn(
-        "@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6",
-        className
-      )}
-      {...props}
-    />
-  );
+function CardHeader({ className, ...props }: React.ComponentProps<'div'>) {
+	return (
+		<div
+			data-slot="card-header"
+			className={cn(
+				'@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6',
+				className,
+			)}
+			{...props}
+		/>
+	);
 }
 
-function CardTitle({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card-title"
-      className={cn("leading-none font-semibold", className)}
-      {...props}
-    />
-  );
+function CardTitle({ className, ...props }: React.ComponentProps<'div'>) {
+	return (
+		<div
+			data-slot="card-title"
+			className={cn('leading-none font-semibold', className)}
+			{...props}
+		/>
+	);
 }
 
-function CardDescription({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card-description"
-      className={cn("text-muted-foreground text-sm", className)}
-      {...props}
-    />
-  );
+function CardDescription({ className, ...props }: React.ComponentProps<'div'>) {
+	return (
+		<div
+			data-slot="card-description"
+			className={cn('text-muted-foreground text-sm', className)}
+			{...props}
+		/>
+	);
 }
 
-function CardAction({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card-action"
-      className={cn(
-        "col-start-2 row-span-2 row-start-1 self-start justify-self-end",
-        className
-      )}
-      {...props}
-    />
-  );
+function CardAction({ className, ...props }: React.ComponentProps<'div'>) {
+	return (
+		<div
+			data-slot="card-action"
+			className={cn(
+				'col-start-2 row-span-2 row-start-1 self-start justify-self-end',
+				className,
+			)}
+			{...props}
+		/>
+	);
 }
 
-function CardContent({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card-content"
-      className={cn("px-6", className)}
-      {...props}
-    />
-  );
+function CardContent({ className, ...props }: React.ComponentProps<'div'>) {
+	return (
+		<div
+			data-slot="card-content"
+			className={cn('px-6', className)}
+			{...props}
+		/>
+	);
 }
 
-function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
-  return (
-    <div
-      data-slot="card-footer"
-      className={cn("flex items-center px-6 [.border-t]:pt-6", className)}
-      {...props}
-    />
-  );
+function CardFooter({ className, ...props }: React.ComponentProps<'div'>) {
+	return (
+		<div
+			data-slot="card-footer"
+			className={cn('flex items-center px-6 [.border-t]:pt-6', className)}
+			{...props}
+		/>
+	);
 }
 
 export {
-  Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardAction,
-  CardDescription,
-  CardContent,
+	Card,
+	CardAction,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
 };
