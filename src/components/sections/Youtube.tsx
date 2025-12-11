be25f95ea0d32927e5bfb0afe7df8abc8c042e44
Vlad Bibire <vladbbr1@gmail.com>
2025-12-11 13:54:34 +0200

    Youtube logo

--- src/components/sections/Youtube.tsx
+++ src/components/sections/Youtube.tsx
@@ -1,5 +1,6 @@
 'use client';
 import Text from '@/components/ui/text';
+import { FaYoutube } from 'react-icons/fa';
 import YouTube from 'react-youtube';
 import TightContentLayout from '../layout/TightContentLayout';
 import PatternSection from './PatternSection';
@@ -7,12 +8,19 @@ import PatternSection from './PatternSection';
 export default function Youtube() {
 	return (
 		<PatternSection
-			className="flex flex-col items-center py-16"
+			className="flex flex-col items-center py-20"
 			patternClassName="opacity-5"
 		>
 			<TightContentLayout>
-				<Text type="h2" className="text-center">
-					As seen on YouTube
+				<Text
+					type="h2"
+					className="flex items-center justify-center gap-3"
+				>
+					As seen on
+					<span className="flex gap-2 items-center">
+						<FaYoutube />
+						YouTube
+					</span>
 				</Text>
 				<YouTube
 					className="mt-10"
