7b77a17e329c72806b080be11468c019f6301db0
Vlad Bibire <vladbbr1@gmail.com>
2025-12-20 12:58:05 +0200

    More tweaks

--- src/components/sections/Youtube.tsx
+++ src/components/sections/Youtube.tsx
@@ -9,7 +9,7 @@ import PatternSection from './PatternSection';
 export default function Youtube() {
 	return (
 		<PatternSection
-			className="flex flex-col items-center py-20"
+			className="flex flex-col items-center md:py-20 md:pb-20 py-16 pb-8"
 			patternClassName="opacity-5 dark:invert"
 		>
 			<TightContentLayout>
