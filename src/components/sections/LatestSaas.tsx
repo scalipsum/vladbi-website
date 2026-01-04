105487f870105556bdad3419a79944da100c1444
Vlad Bibire <vladbbr1@gmail.com>
2026-01-04 23:33:20 +0200

    Update projects section

--- src/components/sections/LatestSaas.tsx
+++ src/components/sections/LatestSaas.tsx
@@ -10,37 +10,32 @@ export default async function LatestSaas() {
 	const latestSaasProduct = await getLatestSaasProduct();
 	const latestAutomationProduct = await getLatestAutomationProduct();
 
-	if (!latestSaasProduct || !latestAutomationProduct) {
-		return (
-			<section className="mt-48">
-				<Text type="h2">Latest digital product</Text>
-				<Text className="mt-8">No digital products available yet.</Text>
-			</section>
-		);
-	}
-
 	return (
 		<section>
 			<Text type="h2" className="text-center">
 				Building products people love
 			</Text>
 
-			<SaasPreviewCard
-				title={latestSaasProduct.title}
-				subtitle={latestSaasProduct.subTitle}
-				description={latestSaasProduct.description}
-				href={`/products/${latestSaasProduct.slug}`}
-				backgroundImageUrl={latestSaasProduct.coverImage ?? ''}
-				previewImageUrl={latestSaasProduct.verticalImage ?? ''}
-				className="mt-8 md:mt-24"
-			/>
+			{latestSaasProduct && (
+				<SaasPreviewCard
+					title={latestSaasProduct.title}
+					subtitle={latestSaasProduct.subTitle}
+					description={latestSaasProduct.description}
+					href={`/products/${latestSaasProduct.slug}`}
+					backgroundImageUrl={latestSaasProduct.coverImage ?? ''}
+					previewImageUrl={latestSaasProduct.verticalImage ?? ''}
+					className="mt-8 md:mt-32"
+				/>
+			)}
 
-			<AutomationPreviewCard
-				title={latestAutomationProduct.title}
-				subtitle={latestAutomationProduct.subTitle}
-				description={latestAutomationProduct.description}
-				href={`/products/${latestAutomationProduct.slug}`}
-			/>
+			{latestAutomationProduct && (
+				<AutomationPreviewCard
+					title={latestAutomationProduct.title}
+					subtitle={latestAutomationProduct.subTitle}
+					description={latestAutomationProduct.description}
+					href={`/products/${latestAutomationProduct.slug}`}
+				/>
+			)}
 
 			<div className="w-full flex justify-center mt-20">
 				<Button asChild className="!px-6">
