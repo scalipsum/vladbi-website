de14f9c667410f9a6e99c871279ac31efd89193f
Vlad Bibire <vladbbr1@gmail.com>
2026-01-06 20:26:08 +0200

    Fix header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -31,7 +31,7 @@ export default function Header({ title, subTitle, className }: HeaderProps) {
 	return (
 		<div
 			className={cn(
-				'relative h-82 w-full flex flex-col items-center justify-center max-w-[1920px] mx-auto overflow-hidden',
+				'relative h-82 w-full flex flex-col items-center justify-center max-w-[2100px] mx-auto',
 				className,
 			)}
 		>
